7a43933fc4a0871b922d1c621613e41a92585a27
Tamas Pasztor <tomcsi@tomcsi.hu>
2016-05-09 10:29:11 +0200

    Change final elasticsearch recipe

--- recipes/elasticsearch.rb
+++ recipes/elasticsearch.rb
@@ -7,11 +7,23 @@
 elasticsearch_configure 'elasticsearch' do
   allocated_memory '512m'
   configuration ({
-    'network.host' => '0.0.0.0'
+    'network.host' => '0.0.0.0',
+    'discovery.type' => 'ec2',
+    'discovery.ec2.groups' => 'elasticsearch',
+    'cloud.aws.region' => 'eu-west-1'
   })
 end
 
 elasticsearch_plugin 'hq' do
   url 'royrusso/elasticsearch-HQ'
   action :install
+end
+
+elasticsearch_plugin 'kopf' do
+  url 'lmenezes/elasticsearch-kopf'
+  action :install
+end
+
+elasticsearch_service 'elasticsearch' do
+  action [:enable, :start]
 end
